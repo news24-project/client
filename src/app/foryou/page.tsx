34c8ce0f0440d75c54c4f755970a7211a6ce5263
Nurken <nurkenqaldybaev2001@gmail.com>
2025-08-25 12:01:24 +0500

    .

--- src/app/foryou/page.tsx
+++ src/app/foryou/page.tsx
@@ -1,4 +1,5 @@
 import CategoryHeader from "@/components/CategoryHeader";
+import NewsItem from "@/components/LocalNews";
 
 import React from "react";
 
@@ -24,6 +25,23 @@ const ForYou = () => {
           title="Sports"
           icon="⚽"
           categories={["Football", "Basketball", "Tennis", "Cycling"]}
+        />
+      </div>
+      <div>
+        <NewsItem
+          source="BBC News"
+          sourceIcon="https://upload.wikimedia.org/wikipedia/commons/e/e4/BBC_News_2019.svg"
+          title="Yangi texnologiyalar kelajakka qanday ta’sir qiladi?"
+          time="2 soat oldin"
+          image="https://picsum.photos/200/300"
+        />
+
+        <NewsItem
+          source="CNN"
+          sourceIcon="https://upload.wikimedia.org/wikipedia/commons/b/b1/CNN.svg"
+          title="Dunyo bo‘ylab ob-havo o‘zgarishi kuchaymoqda"
+          time="1 kun oldin"
+          image="https://picsum.photos/200/300"
         />
       </div>
     </div>
